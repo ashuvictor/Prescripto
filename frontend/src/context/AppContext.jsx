4b6c0e2cb8dd088095230f9d2a22c7b9a312a0ae
Ashu Singh <ashusingh@Ashus-MacBook-Air.local>
2024-11-25 16:28:54 +0530

    bug fixes

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,18 +1,14 @@
-import { createContext } from "react";
+import React, { createContext } from "react";
 import { doctors } from "../assets/assets";
 
 export const AppContext = createContext();
 
-const AppContextProvider = (props) => {
+const AppContextProvider = ({ children }) => {
   const value = {
     doctors,
   };
 
-  return (
-    <AppContext.Provider value={value}>
-      {props.children}
-    </AppContext.Provider>
-  );
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export default AppContextProvider;
