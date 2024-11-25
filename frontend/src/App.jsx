5c39831546743c3990fe5d44d0a70965fb6c2747
Ashu Singh <ashusingh@Ashus-MacBook-Air.local>
2024-11-25 08:54:05 +0530

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import MyAppointments from "./pages/MyAppointments";
+import MyProfile from "./pages/MyProfile";
+import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div className="mx-4 sm:mx[10%]">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appointment/:docId" element={<Appointment />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
