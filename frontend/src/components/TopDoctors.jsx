8b8bad2f7be273458e6111ef408048f797d0c0e3
Ashu Singh <ashusingh@Ashus-MacBook-Air.local>
2024-11-25 15:20:02 +0530

    added a context

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { assets, doctors } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+
 const TopDoctors = () => {
-  const naigate=useNavigate();
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium">Top Doctors To Book</h1>
@@ -13,7 +14,9 @@ const TopDoctors = () => {
         {doctors.slice(0, 12).map((item, index) => (
           <div
             key={index}
-            onClick={()=>{naigate(`appointement/${item._id}`)}}
+            onClick={() => {
+              navigate(`appointement/${item._id}`);
+            }}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition all duration-500"
           >
             <img src={item.image} alt="" className="bg-blue-50" />
@@ -24,11 +27,13 @@ const TopDoctors = () => {
               </div>
             </div>
             <p className="text-gray-900 text-lg font-medium">{item.name}</p>
-            <p className="text-gray-500 text-sm">{item.speciality}</p>
+            <p className="text-gray-500 text-sm mt-2">{item.speciality}</p>
           </div>
         ))}
       </div>
-      <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">More...</button>
+      <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">
+        More...
+      </button>
     </div>
   );
 };
