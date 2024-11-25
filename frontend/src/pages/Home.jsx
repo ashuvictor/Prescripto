87e4128a24a4e8588e3fe755bce9409d16636139
Ashu Singh <ashusingh@Ashus-MacBook-Air.local>
2024-11-25 17:06:27 +0530

    added the banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Header from '../components/Header'
-import SpecialityMenu from '../components/SpecialityMenu'
-import TopDoctors from "../components/TopDoctors"
+import React from "react";
+import Header from "../components/Header";
+import SpecialityMenu from "../components/SpecialityMenu";
+import TopDoctors from "../components/TopDoctors";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
     <div>
-      <Header/>
-      <SpecialityMenu />      
-      <TopDoctors/>
+      <Header />
+      <SpecialityMenu />
+      <TopDoctors />
+      <Banner />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
