042e6d2cb6b3101234d9fe4cf81b05a8f45ab0af
Ashu Singh <ashusingh@Ashus-MacBook-Air.local>
2024-11-26 12:15:19 +0530

    added backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,22 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
+import "dotenv/config";
+
+// APP CONFIG
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middlewares
+
+app.use(express.json());
+app.use(cors());
+
+//endpoint
+
+app.get("/", (req, res) => {
+  res.send("Api working great");
+});
+
+app.listen(port,()=>{
+    console.log("Sever started",port)
+})
