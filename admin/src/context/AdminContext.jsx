c57f15c058e7153f67d334ae8a104f61bcd60463
Ashu Singh <ashusingh@Ashus-MacBook-Air.local>
2024-11-27 13:32:00 +0530

    Added Context

---
+++ admin/src/context/AdminContext.jsx
@@ -0,0 +1,11 @@
+import React, { createContext } from "react";
+
+export const AdminContext = createContext();
+
+const AdminContextProvider = (props) => {
+  const value = {};
+
+  return <AdminContext.Provider value={value}>{props.childern}</AdminContext.Provider>;
+};
+
+export default AdminContextProvider;
---
+++ admin/src/context/DoctorContext.jsx
@@ -0,0 +1,11 @@
+import React, { createContext } from "react";
+
+export const DoctorContext = createContext();
+
+const DoctorContextProvider = (props) => {
+  const value = {};
+
+  return <DoctorContext.Provider value={value}>{props.childern}</DoctorContext.Provider>;
+};
+
+export default DoctorContextProvider;
